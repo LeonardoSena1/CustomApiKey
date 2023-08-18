cb32e15653979a68643c271285775b942228cc94
Leonardo Sena <leonardo_sena1@hotmail.com>
2023-08-18 12:29:38 -0300

    Add project files.

---
+++ CustomApiKey/Attribute/ApiKeyAttribute.cs
@@ -0,0 +1,59 @@
+﻿using CustomApiKey.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace CustomApiKey.Attribute
+{
+    [AttributeUsage(validOn: AttributeTargets.Class)]
+    public class ApiKeyAttribute : System.Attribute, IAsyncActionFilter
+    {
+        private const string APIKEYNAME = "ApiKey";
+        private const string NotProvided = "Api Key was not provided";
+        private const string NotValid = "Api Key is not valid";
+        private const string ContentType = "application/json";
+        private const int StatusCode = (int)HttpStatusCode.Unauthorized;
+        private const bool Success = false;
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            if (!context.HttpContext.Request.Headers.TryGetValue(APIKEYNAME, out var extractedApiKey))
+            {
+                context.Result = new ContentResult()
+                {
+                    StatusCode = StatusCode,
+                    ContentType = ContentType,
+                    Content = JsonConvert.SerializeObject(
+                        new BaseResponse
+                        {
+                            Success = Success,
+                            Message = NotProvided,
+                        })
+                };
+                return;
+            }
+
+            var appSettings = context.HttpContext.RequestServices.GetRequiredService<IConfiguration>();
+
+            var apiKey = appSettings.GetValue<string>(APIKEYNAME);
+
+            if (!apiKey.Equals(extractedApiKey))
+            {
+                context.Result = new ContentResult()
+                {
+                    StatusCode = StatusCode,
+                    ContentType = ContentType,
+                    Content = JsonConvert.SerializeObject(
+                        new BaseResponse
+                        {
+                            Success = Success,
+                            Message = NotValid,
+                        })
+                };
+                return;
+            }
+
+            await next();
+        }
+    }
+}
---
+++ CustomApiKey/Models/BaseResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace CustomApiKey.Models
+{
+    public class BaseResponse
+    {
+        public bool Success { get; set; }
+        public string? Message { get; set; }
+        public string? Runtime { get; set; }
+        public List<Object> Data { get; set; }
+        public int CountData { get; set; }
+    }
+}
